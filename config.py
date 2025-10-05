8970745d2e6b03eb2374a69c9d829c8ffc82d904
khushnazkhan <khushnazkhan0786@gmail.com>
2025-10-05 22:50:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🚀 TEST APP - WORKING")
+st.write("If you see this, Streamlit is working!")
+
+if st.button("Click me"):
+    st.success("Button clicked! Everything is working!")
